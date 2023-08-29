ebcb0c7d982f681f14bb25f89b56eb60d9a2ad60
sergioeddy1 <eduardohedz1@hotmail.com>
2023-08-29 14:33:01 -0600

    Primeros cambios dentro del servicio

--- src/app/countries/services/countries/countries.service.ts
+++ src/app/countries/services/countries/countries.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../../interfaces/countries.interfacs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private _region: Region[] = [];
+
   constructor() { }
+
+
 }
